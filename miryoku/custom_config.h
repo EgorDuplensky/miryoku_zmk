03776f146beaa59012c3341fa8e09966d40c0ddd
Egor Duplensky <egor.duplensky@gmail.com>
2024-07-20 13:02:14 +0200

    Glove80 add extra keys for cirillic layout

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,28 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define XXX &none
+
+#define MIRYOKU_ALPHAS_QWERTY
+#define MIRYOKU_NAV_VI
+
+#define MIRYOKU_LAYER_BASE \
+&kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
+U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SEMI),   \
+U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
+U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
+
+#define MIRYOKU_LAYOUTMAPPING_GLOVE80( \
+     K00, K01, K02, K03, K04,                                    K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,                                    K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,                                    K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,                                    K35, K36, K37, N38, N39 \
+) \
+      XXX  XXX  XXX  XXX  XXX                                               XXX  XXX  XXX  XXX  XXX \
+      XXX  XXX  XXX  XXX  XXX  XXX                                     XXX  XXX  XXX  XXX  XXX  XXX \
+ &kp RBRC  K00  K01  K02  K03  K04                                     K05  K06  K07  K08  K09  &kp LBRC \
+&kp MINUS  K10  K11  K12  K13  K14                                     K15  K16  K17  K18  K19  &kp SQT \
+      XXX  K20  K21  K22  K23  K24  XXX  XXX  XXX       XXX  XXX  XXX  K25  K26  K27  K28  K29  XXX \
+      XXX  XXX  XXX  XXX  XXX       K32  K33  K34       K35  K36  K37       XXX  XXX  XXX  XXX  XXX
+
+#define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_GLOVE80
